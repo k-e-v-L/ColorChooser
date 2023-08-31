7fd3cd2e5416c4eb1e33d394c6f2db5fb4d6008c
k-e-v-L <112248826+k-e-v-l@users.noreply.github.com>
2023-08-31 15:43:56 +0300

    We have a JFrame, label. Opens a GUI, where we have a button "choose a color" next to the button we have random text, with green background color.

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // JColorChooser = A GUI mechanism that lets user choose a color
+
+        new MyFrame();
+    }
+
+}
---
+++ MyFrame.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
+    JLabel label;
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Choose a color");
+        button.addActionListener(this);
+
+        label = new JLabel();
+        label.setBackground(Color.green);
+        label.setText("Random text");
+        label.setFont(new Font("MV Boli",Font.PLAIN,54));
+        label.setOpaque(true);
+
+        this.add(button);
+        this.add(label);
+        this.pack();
+        this.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
